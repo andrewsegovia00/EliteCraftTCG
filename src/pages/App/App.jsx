7ab304096aca01a556b4e839a642af478ac2eb4a
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-21 16:38:02 -0700

    Add Home & Dashboard pages to App

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import "Dashboard" from "../Dashboard/Dashboard.jsx"
-import "Home" from "../Home/Home"
+// import { useState } from 'react'
+import Dashboard from "../Dashboard/Dashboard.jsx"
+import Home from "../Home/Home"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Home />
+      <Dashboard />
     </>
   )
 }
