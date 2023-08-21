ccc81d4285c66a2515a4857d3fba8141ce4d6969
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-21 16:22:25 -0700

    Add new Dashboard.jsx and Home.jsx pages Add Home and Dashboard testing files and useStates

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './Home.css'
+
+export default function Home() {
+    const [user, setUser] = useState(null)
+
+    function handleSubmit(evt) {
+        const name = evt.target.value;
+        setUser(name)
+    }
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <div>{user}</div>
+      <form onSubmit={handleSubmit}>
+        <input name='name' value=''/>
+        <button type='submit'></button>
+      </form>
+    </>
+  )
+}
+
