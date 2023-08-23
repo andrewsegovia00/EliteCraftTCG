c343d13b0973818e162fbf13060c318b5079f921
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-23 10:23:20 -0700

    Changed vite's settings from js files being dished out as modules to commonjs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 require('dotenv').config();
 require('./config/database');
@@ -10,10 +10,12 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = import.meta.env.PORT || 3001;
+// const port = import.meta.env.PORT || 3001;
+const port = process.env.PORT || 3001;
+
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
@@ -22,3 +24,4 @@ app.get('/*', function(req, res) {
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);
 });
+
