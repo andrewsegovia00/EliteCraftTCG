f2568fc4c70abd2212805316f59d919ddd33074b
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-23 16:44:54 -0700

    Login implementation almost finished, added utilities, routing, and components to render a sign up and login page, and database is connected and user is displaying on mongoDB

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,48 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+import AuthPage from '../AuthPage/AuthPage';
 import './Home.css'
 
 export default function Home() {
-    const [name, setName] = useState('');
-    const [user, setUser] = useState(null)
+  const [user, setUser] = useState(getUser());
+    // const [name, setName] = useState('');
+    // const [user, setUser] = useState(null)
+    return (
+      <main className="App">
+        { user ?
+            <>
+              {/* <NavBar user={user} setUser={setUser} /> */}
+              <Routes>
+                {/* Route components in here */}
+                {/* <Route path="/orders/new" element={<NewOrderPage />} /> */}
+                {/* <Route path="/orders" element={<OrderHistoryPage />} /> */}
+              </Routes>
+            </>
+            :
+            <AuthPage setUser={setUser} />
+        }
+      </main>
+    );
+  }
+//     function handleSubmit(evt) {
+//       evt.preventDefault();
+//       setUser(name);
+//     }
   
-    function handleSubmit(evt) {
-      evt.preventDefault();
-      setUser(name);
-    }
-  
-    function handleNameChange(evt) {
-      setName(evt.target.value);
-    }
+//     function handleNameChange(evt) {
+//       setName(evt.target.value);
+//     }
 
-    
-
-  return (
-    <>
-      <h1>Home Page</h1>
-      <div>{user}</div>
-      <form onSubmit={handleSubmit}>
-        <input name='name' value={name} onChange={handleNameChange} />
-        <button type='submit'></button>
-      </form>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <h1>Home Page</h1>
+//       <div>{user}</div>
+//       <form onSubmit={handleSubmit}>
+//         <input name='name' value={name} onChange={handleNameChange} />
+//         <button type='submit'></button>
+//       </form>
+//     </>
+//   )
+// }
 
