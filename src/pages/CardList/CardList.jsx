e03de4f98dcb37b598ca2515bdae8e0ec35c9841
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-25 11:39:56 -0700

    Added comments to CardList

--- src/pages/CardList/CardList.jsx
+++ src/pages/CardList/CardList.jsx
@@ -17,10 +17,29 @@ export default function CardList() {
   }, [setId]);
 
   return (
+    // <div>
+    //   <h1>{data.set.name}</h1>
+    //   <h3>Cards in this Set:</h3>
+    //   <ul>
+    //     {data.cards.map((card) => (
+    //       <div key={card._id}>
+    //         <h1>{card.name}</h1>
+    //         <img src={`${card.imageUrl}`} />
+    //         <p>{card.number}</p>
+    //         <p>{card.set}</p>
+    //         <p>{card.typeImg}</p>
+    //         <ul>standard: {card.legality.standard}</ul>
+    //         <ul>expanded {card.legality.expanded}</ul>
+    //         <ul>unlimited {card.legality.unlimited}</ul>
+    //       </div>
+    //     ))}
+    //   </ul>
+    // </div>
     <div className="container">
       <div className="mb-4">
         <strong className="is-size-4">{data.set.name}</strong>
-        <small>({data.set.setCode})</small>
+        <img src={`${data.set.imageUrl}`} />
+        {/* <small>({data.set.setCode})</small> */}
       </div>
       <div className="gallery columns is-mobile">
         {data.cards.map((card) => (
@@ -28,16 +47,17 @@ export default function CardList() {
             className="column is-half-mobile is-one-third-tablet is-one-fifth-desktop"
             key={card._id}
           >
-            <a href={`/card/${card.slug}/${card.setCode}-${card.number}`}>
+            {/* <a href={`/card/${card.slug}/${card.setCode}-${card.number}`}> */}
               <img
                 className="card-image hvr-grow"
                 src={card.imageUrl}
                 alt={card.name}
               />
-            </a>
+            {/* </a> */}
           </div>
         ))}
       </div>
     </div>
   );
 }
+
