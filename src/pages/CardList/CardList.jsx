f0d310cfac5f7503a2ecaadb47e126de8c07133d
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-25 11:32:32 -0700

    Add styling for the details page of every set

--- src/pages/CardList/CardList.jsx
+++ src/pages/CardList/CardList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getAllCardsBySetId } from '../../utilities/cards';
+import './CardList.css'
 
 export default function CardList() {
   const { setId } = useParams(); 
@@ -16,24 +17,27 @@ export default function CardList() {
   }, [setId]);
 
   return (
-    <div>
-      <h1>{data.set.name}</h1>
-      <img src={`${data.set.imageUrl}`} />
-      <h3>Cards in this Set:</h3>
-      <ul>
+    <div className="container">
+      <div className="mb-4">
+        <strong className="is-size-4">{data.set.name}</strong>
+        <small>({data.set.setCode})</small>
+      </div>
+      <div className="gallery columns is-mobile">
         {data.cards.map((card) => (
-          <div key={card._id}>
-            <h1>{card.name}</h1>
-            <img src={`${card.imageUrl}`} />
-            <p>{card.number}</p>
-            <p>{card.set}</p>
-            <p>{card.typeImg}</p>
-            <ul>standard: {card.legality.standard}</ul>
-            <ul>expanded {card.legality.expanded}</ul>
-            <ul>unlimited {card.legality.unlimited}</ul>
+          <div
+            className="column is-half-mobile is-one-third-tablet is-one-fifth-desktop"
+            key={card._id}
+          >
+            <a href={`/card/${card.slug}/${card.setCode}-${card.number}`}>
+              <img
+                className="card-image hvr-grow"
+                src={card.imageUrl}
+                alt={card.name}
+              />
+            </a>
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
