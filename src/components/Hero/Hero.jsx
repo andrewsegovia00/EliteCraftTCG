4eecac217a1072e9758cf6ae083ef49eb31f4005
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-25 22:07:59 -0700

    Hero component CSS Update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,21 @@
-
 import './Hero.css'
 
 export default function Hero() {
 
     return (
     <>
-    <div style={{display: 'flex', justifyContent: 'center' }}>
-      <div style={{display: 'inline'}}>
-        <h1>
-          Create & Build
-        </h1>
-      </div>
-      <div style={{display: 'inline'}}>
-        <h3>
-          Build your dream decks and share with the TCG community. Choose from a range of booster packs and get feedback from others on whether or not your deck is balanced or a match for the meta
+<div className="container">
+  <div className="column1">
+    <h1>Create & Build</h1>
+    <h3>
+      Build your dream decks and share with the TCG community. Choose from a range of booster packs and get feedback from others on whether or not your deck is balanced or a match for the meta.
+    </h3>
+  </div>
+  <div className="column2">
+      <img src="https://images.pokemontcg.io/xy3/55.png" />
+  </div>
+</div>
 
-        </h3>
-      </div>
-    </div>
     </>
     );
   }
