4fb311c2cdda764874efb1c2deba20fe8912eff1
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-08-29 04:42:23 -0700

    Hero Animation added Hero Component revamped

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,26 @@
 import './Hero.css'
+import charizardCard from "../../assets/charizardCard.png"
+import pokekidCard from "../../assets/pokekidCard.png"
+import cinderaceCard from "../../assets/cinderaceCard.png"
 
 export default function Hero() {
 
     return (
     <>
-  <div className="container">
-    <div className="column1">
-      <h1>Create & Build</h1>
-      <h3>
-        Build your dream decks and share with the TCG community. Choose from a range of booster packs and get feedback from others on whether or not your deck is balanced or a match for the meta.
-      </h3>
-    </div>
-    <div className="column2">
-        <img src="https://images.pokemontcg.io/xy3/55.png" />
-    </div>
-  </div>
-
+      <div className="hero">
+          <div className="buildText">build</div>
+          <div className="circle-glass">
+              <div className="circle1"></div>
+              <div className="circle2"></div>
+          </div>
+          <div className="pokemonCardSet">
+              <img src={pokekidCard} className="card2 pokemonCard1" />
+              <img src={charizardCard} className="card2 pokemonCard2" />
+              <img src={cinderaceCard} className="card2 pokemonCard3" />
+          </div>
+          <div className="subText">Build your dream decks and share with the TCG community. Choose from a range of booster packs and get feedback from others on whether or not your deck is balanced or a match for the meta.</div>
+          <button className="Account">create account</button>
+      </div>
     </>
     );
   }
